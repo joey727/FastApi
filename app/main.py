289f898344b619c8b8f923a0031d6de852646429
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-05-13 20:44:10 +0000

    main fastapi program

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "welcome to my [first] api"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"posts": "view all posts here"}
