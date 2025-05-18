e23063484993d74713a73d665ea6fbcbbaecf923
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-05-18 14:49:22 +0000

    user schemas using pydantic lib

---
+++ app/schemas.py
@@ -0,0 +1,36 @@
+
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+
+
+class Post(BaseModel):
+    id: int
+    title: str
+    content: str
+    is_published: bool = True
+
+
+class PostReturn(Post):
+    pass
+
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    user_id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
