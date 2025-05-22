6b187df65654cfd2ee62fa52598d4208ef6bb433
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-05-22 11:38:05 +0000

    main file clean up

--- app/main.py
+++ app/main.py
@@ -1,12 +1,7 @@
-from typing import List
-from fastapi import Depends, FastAPI, HTTPException, status, Response
-from psycopg2 import connect
-from psycopg2.extras import RealDictCursor
-
+from fastapi import FastAPI
 from app.routers import auth
-from .database import engine, get_db
-from sqlalchemy.orm import Session
-from app import models, utils
+from .database import engine
+from app import models
 from .routers import user, post
 
 
@@ -14,15 +9,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-try:
-    conn = connect(host="localhost", database="myFastApi",
-                   user="postgres", password="rootAdmin", cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("database connection succesfull")
-except Exception as err:
-    print("problem with connection")
-    print(err)
 
 app.include_router(user.router)
 app.include_router(post.router)
