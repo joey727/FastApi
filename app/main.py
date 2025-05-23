da770b3d319ca04831876291f57db92c3a07b927
Joey727 <joshuasackeyjr.1124@gmail.com>
2025-05-23 21:11:32 +0000

    set router for vote system

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from app.routers import auth
+from app.routers import auth, vote, user, post
 from .database import engine
 from app import models
-from .routers import user, post
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -13,3 +12,4 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
